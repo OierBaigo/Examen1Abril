3007378a8457f76f7b70df91e4784d023f93dd67
Oier Baigorrotegui Gil <oierbaigo@gmail.com>
2025-04-01 12:50:35 +0200

    [refactor] - Dos test para el caso de un producto  para el de varios

--- tests/ShoppingListTest.php
+++ tests/ShoppingListTest.php
@@ -16,11 +16,23 @@ class ShoppingListTest extends TestCase
         $shoppingList = "";
         $cart = new ShoppingList($shoppingList);
 
+        $result = $cart->cart("añadir pan");
+
+        $this->assertEquals("pan x1", $result);
+    }
+
+    /**
+     * @test
+     */
+    public function givenAddInstrucctionAndMultipleProductsReturnsListWithProduct():void
+    {
+        $shoppingList = "";
+        $cart = new ShoppingList($shoppingList);
+
         $cart->cart("añadir leche");
         $result = $cart->cart("añadir pan");
 
         $this->assertEquals("leche x1, pan x1", $result);
     }
-
 
 }
